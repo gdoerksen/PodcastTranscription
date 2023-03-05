fe06223fc9a56a900be4348eafae6440b037738c
Graham Doerksen <46031209+gdoerksen@users.noreply.github.com>
2023-03-05 10:35:56 -0700

    Added rttm loader

---
+++ src/utils/rttm_loader.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def load_rttm_file( rttm_file_path: Path )-> list[int, int, int]:
+    """
+    Load a speaker timetsamp .rttm file and return a list of lists of the form: [start_time, end_time, speaker_id]
+    """
+    speaker_ts = []
+    with rttm_file_path.open('r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line_list = line.split(' ')
+            start = int(float(line_list[3]) * 1000)
+            end = start + int(float(line_list[4]) * 1000)
+            speaker_id = int(line_list[7].split('_')[-1])
+            speaker_ts.append([start, end, speaker_id])
+
+    return speaker_ts
